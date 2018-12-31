609c1292dbb8d569e2835c0d18d889495ab3908f
erichstuder <richu_@hotmail.com>
2018-12-31 12:39:54 +0100

    startup speed improvement

--- forschungAktuell/forschungAktuell.m
+++ forschungAktuell/forschungAktuell.m
@@ -3,10 +3,13 @@
 %fwrite(fid, a);
 %fclose(fid);
 
+disp('Get links to broadcastings.');
 url = "https://www.podcast.de/podcast/11600/archiv/?podcast/11600/archiv/=&seite=";
 cnt = 1;
+nrOfPages = 29;
 clear links;
-for n = 1:29
+for n = 1:nrOfPages
+    disp(['Parsing page ' num2str(n) ' of ' num2str(nrOfPages) ' pages.']);
     siteText = urlread([url num2str(n)]);
     siteLines = strsplit(siteText, "\n");
     for n = 1:numel(siteLines)
@@ -19,42 +22,42 @@ for n = 1:29
     end
 end
 
-cnt = 1;
-clear mp3Links;
-for n = 1:numel(links)
-  siteText = urlread(links{n});
-  siteLines = strsplit(siteText, "\n");
-  for n = 1:numel(siteLines)
-    line = siteLines{n};
-    if strfind(line, "<meta name=\"twitter:player:stream\" content=\"http://podcast-mp3.dradio.de/podcast/")
-      range = strfind(line, "\"");
-      mp3Links{cnt} = line(range(3)+1:range(4)-1);
-      cnt++;
-    end
-  end    
-end
-
 if exist("doneMp3", "file")
   load("doneMp3");
 else
   doneMp3 = {};
 end
 
-for n = numel(mp3Links):-1:1
-  n
-  if numel(cell2mat(strfind(doneMp3, mp3Links(n)))) == 0
-    [audio, ~, ~]=urlread(mp3Links{n});
-    fid = fopen("audio.mp3", "w");
-    fwrite(fid, audio);
-    fclose(fid);
-    
-    system("audio.mp3");
-    
-    input("press enter for next");
-    
-    doneMp3 = [doneMp3 mp3Links(n)];
-    save("doneMp3", "doneMp3");
-  end
+disp('');
+disp('Get link to mp3 of broadcasting and play.');
+for n = numel(links):-1:1
+  disp(['Parsing link ' num2str(n) ' of ' num2str(numel(links)) ' links.']);
+  siteText = urlread(links{n});
+  siteLines = strsplit(siteText, "\n");
+  for n = 1:numel(siteLines)
+    line = siteLines{n};
+    if strfind(line, "<meta name=\"twitter:player:stream\" content=\"http://podcast-mp3.dradio.de/podcast/")
+      range = strfind(line, "\"");
+      mp3Link = line(range(3)+1:range(4)-1);
+      
+      if numel(cell2mat(strfind(doneMp3, mp3Link))) == 0
+        disp('Downloading the mp3 file.')
+        [audio, ~, ~]=urlread(mp3Link);
+        fid = fopen("audio.mp3", "w");
+        fwrite(fid, audio);
+        fclose(fid);
+        
+        system("audio.mp3");
+        
+        input("Press enter for next.");
+        
+        doneMp3 = [doneMp3 mp3Link];
+        save("doneMp3", "doneMp3");
+      else
+        disp('Broadcast already heard.')
+      end
+    end
+  end    
 end
 
 
