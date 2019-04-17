22df76e35c6819a1da046646b31709f22031830a
erichstuder <richu_@hotmail.com>
2019-04-17 19:54:59 +0200

    initial commit

---
+++ watch/watch/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace watch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.time = new System.Windows.Forms.TextBox();
+            this.dummy = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.BackColor = System.Drawing.SystemColors.Window;
+            this.time.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.time.Font = new System.Drawing.Font("Kristen ITC", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.Location = new System.Drawing.Point(-2, 0);
+            this.time.Name = "time";
+            this.time.ReadOnly = true;
+            this.time.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.time.Size = new System.Drawing.Size(127, 25);
+            this.time.TabIndex = 0;
+            this.time.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.time.MouseLeave += new System.EventHandler(this.time_MouseLeave);
+            this.time.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.time_MouseDoubleClick);
+            this.time.MouseEnter += new System.EventHandler(this.time_MouseEnter);
+            // 
+            // dummy
+            // 
+            this.dummy.Location = new System.Drawing.Point(-2, 26);
+            this.dummy.Name = "dummy";
+            this.dummy.Size = new System.Drawing.Size(100, 20);
+            this.dummy.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(124, 23);
+            this.Controls.Add(this.dummy);
+            this.Controls.Add(this.time);
+            this.MaximumSize = new System.Drawing.Size(132, 57);
+            this.MinimumSize = new System.Drawing.Size(132, 57);
+            this.Name = "Form1";
+            this.Opacity = 0.2;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox time;
+        private System.Windows.Forms.TextBox dummy;
+
+
+    }
+}
+
---
+++ watch/watch/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace watch
+{
+    public partial class Form1 : Form
+    {
+        private System.Threading.Timer reduceOpacityTimer;
+        private bool mouseIn = false;
+        private Thread doTextThread;
+        private Thread increaseOpacityThread;
+        //private Thread reduceOpacityThread;
+
+
+        private const double OPACITY_MIN = 0.05;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dummy.Focus();//damit der cursor verschwindet
+
+            Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+            Point point = new Point(screen.Width-this.Width,screen.Height-this.Height);
+
+
+            this.Location = point;
+            this.StartPosition = FormStartPosition.Manual;
+
+            time.ForeColor = Color.White;
+            //damit this.time.BeginInvoke(doText); funktioniert
+
+            doTextThread = new Thread(delegate()
+            {
+                MethodInvoker doText = new MethodInvoker(delegate()
+                {
+                    if (mouseIn == true | Math.Round(this.Opacity,2) > OPACITY_MIN)
+                    {
+
+                        time.ForeColor = Color.Black;
+                        
+                        time.Text = DateTime.Now.ToString();
+
+                        //time.ForeColor = Color.Black;
+                    }
+                    else
+                    {
+                        time.ForeColor = Color.White;
+                        
+                        time.Text = "";
+                    }
+                    //this.Text = this.Opacity.ToString();
+                });
+
+
+                while (this.IsHandleCreated == false) Thread.Sleep(100);
+
+                while (true)
+                {
+                    
+                    this.time.BeginInvoke(doText);
+                    Thread.Sleep(100);
+                }
+            });
+            doTextThread.Start();
+
+        }
+
+        private void time_MouseEnter(object sender, EventArgs e)
+        {
+            mouseIn = true;
+            
+            dummy.Focus();//damit der cursor verschwindet
+
+            MethodInvoker increaseOpacity = new MethodInvoker(delegate()
+            {
+                this.Opacity += 0.05;
+                time.Text = DateTime.Now.ToString();
+                this.Text = DateTime.Now.DayOfWeek.ToString().ToLower();
+            });
+
+            increaseOpacityThread = new Thread(delegate()
+                {
+                    while (this.Opacity < 1)
+                    {
+                        this.time.Invoke(increaseOpacity);
+                        Thread.Sleep(25);
+                    }
+                });
+            increaseOpacityThread.Start();
+        }
+
+        private void time_MouseLeave(object sender, EventArgs e)
+        {
+            mouseIn = false;
+            //mouseWasIn = false;
+            if (reduceOpacityTimer != null)
+            {
+                reduceOpacityTimer.Change(2000,System.Threading.Timeout.Infinite);
+            }
+            else
+            {
+                reduceOpacityTimer = new System.Threading.Timer(doTimer,null,2000,System.Threading.Timeout.Infinite);
+            }
+        }
+
+        private void doTimer(object o)
+        {
+            MethodInvoker reduceOpacity = new MethodInvoker(delegate()
+            {
+                this.Opacity -= 0.05;
+            });
+
+
+
+
+            while (Math.Round(this.Opacity,2) > OPACITY_MIN & mouseIn == false)
+            {   
+                
+                this.time.Invoke(reduceOpacity);
+                Thread.Sleep(50);
+            }
+            //mouseWasIn = false;
+        }
+
+        private void time_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Clipboard.SetText(time.Text.Split(' ')[0]);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try{ doTextThread.Abort(); }
+            catch (Exception) { }
+
+            try { reduceOpacityTimer.Dispose(); }
+            catch (Exception) { }
+
+            try { increaseOpacityThread.Abort(); }
+            catch (Exception) { }
+
+
+        }
+    }
+}
