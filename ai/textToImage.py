4b6f839476beed15ae07e7408fd5c21c1745467c
erichstuder <richu_@hotmail.com>
2019-08-18 09:08:29 +0200

    made it work for windows and some cleanup

--- ai/textToImage.py
+++ ai/textToImage.py
@@ -2,11 +2,12 @@ import sys
 from PIL import Image, ImageDraw, ImageFont #install Pillow to use PIL
 
 img = Image.new('RGB', (1000, 300), color=(255, 255, 255))
+fontSize = 30
 
 if sys.platform.startswith('win'):
-    pass
+    fnt = ImageFont.truetype('arial.ttf', fontSize)
 elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-    fnt = ImageFont.truetype('/usr/share/fonts/truetype/Arial.ttf', 30)
+    fnt = ImageFont.truetype('/usr/share/fonts/truetype/Arial.ttf', fontSize)
 else:
     raise EnvironmentError('Unsupported platform')
 
