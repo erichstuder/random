6579ef9fcfff72032fb86a6ebcb4f26f02505974
erichstuder <richu_@hotmail.com>
2019-09-11 19:49:51 +0200

    some hacking with tasks (not compiling)

--- Altera_Cyclone_II_FPGA_Starter_Board/clock/sourceCode/I2cMaster.v
+++ Altera_Cyclone_II_FPGA_Starter_Board/clock/sourceCode/I2cMaster.v
@@ -6,6 +6,7 @@ module I2cMaster
 	  parameter MaxBytesToRead = 16)
 	(input reset,
 	 input clock,
+	 input start,
 	 input [MaxBytesToSend-1:0] nrOfBytesToSend,
      input [MaxBytesToSend-1:0][7:0] bytesToSend,
 	 input [MaxBytesToRead-1:0] nrOfBytesToRead,
@@ -16,32 +17,71 @@ module I2cMaster
 	 output clockStretchTimeoutReached);
 
 	localparam
-		Start=3'b000,
-		AddressForWrite=3'b001,
-		SendData=3'b010,
-		Restart=3'b011,
-		AddressForRead=3'b100,
-		ReadData=3'b101,
-		Stop=3'b110;
+		Idle=3'b000,
+		Start=3'b001,
+		AddressForWrite=3'b010,
+		SendData=3'b011,
+		Restart=3'b100,
+		AddressForRead=3'b101,
+		ReadData=3'b110,
+		Stop=3'b111;
 	
 	reg[2:0] state;
 	
 	//All delays are made 1ms. This makes it slower but also simpler.
-	localparam Timeout1ms=ClockFrequency/1000; 
+	localparam Timeout1msCount=ClockFrequency/1000; 
 	
 	integer clockStretchTimeoutCounter;
 	integer delayCounter;
 
 	always@(posedge clock or posedge reset)
 	begin
+		if(reset)
+		begin
+			sda = z;
+			scl = z;
+			ready = 1;
+			clockStretchTimeoutReached = 0;
+			state = Idle;
+		end
 		case(state)
+		Idle:
+		begin
+			if(start)
+			begin
+				state = Start;
+			end
+		end
 		Start:
+		begin
+			sda = 0;
+			delayCounter++;
+			state = AddressForWrite;
+		end
 		AddressForWrite:
+		begin
+			state = SendData;
+		end
 		SendData:
+		begin
+			state = Restart;
+		end
 		Restart:
+		begin
+			state = AddressForRead;
+		end
 		AddressForRead:
+		begin
+			state = ReadData;
+		end
 		ReadData:
+		begin
+			state = Stop;
+		end
 		Stop:
+		begin
+			state = Idle;
+		end
 	end
 
 endmodule
