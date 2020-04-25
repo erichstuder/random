38e8613b6bc94f6a3e55791f49850f6a1b52ca31
erichstuder <richu@hotmail.com>
2020-04-25 08:54:36 +0200

    added more leds for better visibility

--- waterDetector/arduinoLeonardoTerminal/arduinoLeonardoTerminal.ino
+++ waterDetector/arduinoLeonardoTerminal/arduinoLeonardoTerminal.ino
@@ -10,6 +10,9 @@ static char inputBuffer[100];
 static unsigned char inputBufferIndex = 0;
 
 static inline void handleInput(void);
+static inline void ledOn(void);
+static inline void ledOff(void);
+static inline void initLed(void);
 static inline void appendToInputBuffer(char ch);
 
 void setup(void) {}
@@ -33,12 +36,10 @@ void loop(void) {
 
 static inline void handleInput(void) {
 	if(strcmp(inputBuffer, "setLedOn") == 0){
-		pinMode(LED_BUILTIN, OUTPUT);
-		digitalWrite(LED_BUILTIN, HIGH);
+		setLedOn();
 	}
 	else if(strcmp(inputBuffer, "setLedOff") == 0){
-		pinMode(LED_BUILTIN, OUTPUT);
-		digitalWrite(LED_BUILTIN, LOW);
+		setLedOff();
 	}
 	else if(strcmp(inputBuffer, "getAdc_A0") == 0){
 		char numberString[4+sizeof('\0')];
@@ -47,6 +48,41 @@ static inline void handleInput(void) {
 		Serial.write('\n');
 	}
 	inputBufferIndex = 0;
+}
+
+static inline void setLedOn(void) {
+	initLed();
+	digitalWrite(0, LOW);
+	digitalWrite(1, HIGH);
+	digitalWrite(2, LOW);
+	digitalWrite(3, HIGH);
+	digitalWrite(5, LOW);
+	digitalWrite(7, HIGH);
+	digitalWrite(9, LOW);
+	digitalWrite(11, HIGH);
+}
+
+static inline void setLedOff(void) {
+	initLed();
+	digitalWrite(0, LOW);
+	digitalWrite(1, LOW);
+	digitalWrite(2, LOW);
+	digitalWrite(3, LOW);
+	digitalWrite(5, LOW);
+	digitalWrite(7, LOW);
+	digitalWrite(9, LOW);
+	digitalWrite(11, LOW);
+}
+
+static inline void initLed(void) {
+	pinMode(0, OUTPUT);
+	pinMode(1, OUTPUT);
+	pinMode(2, OUTPUT);
+	pinMode(3, OUTPUT);
+	pinMode(5, OUTPUT);
+	pinMode(7, OUTPUT);
+	pinMode(9, OUTPUT);
+	pinMode(11, OUTPUT);
 }
 
 static inline void appendToInputBuffer(char ch) {
