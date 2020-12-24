cd3fbb17f03f2fc7dc59f0a2bc2dcfb8d4be912d
erichstuder <richu_@hotmail.com>
2020-12-24 08:56:05 +0100

    filtering the adc values

--- waterDetector/arduinoLeonardoStandAlone/arduinoLeonardoStandAlone.ino
+++ waterDetector/arduinoLeonardoStandAlone/arduinoLeonardoStandAlone.ino
@@ -4,18 +4,36 @@
 #define LED_BUILTIN 13
 
 static bool ledIsOn;
+static uint8_t filter;
+static bool filterFulfilled;
 
 static inline void handleInput(void);
 static inline void setLedOn(void);
 static inline void setLedOff(void);
 static inline void initLed(void);
 
+
 void setup(void) {
 	 ledIsOn = false;
+	 filter = 0;
+	 filterFulfilled = false;
 }
 
 void loop(void) {
 	if(analogRead(A0) > 500){
+		if(filter < 20){
+			filter++;
+		}
+		else{
+			filterFulfilled = true;
+		}
+	}
+	else{
+		filter = 0;
+		filterFulfilled = false;
+	}
+
+	if(filterFulfilled){
 		if(ledIsOn){
 			setLedOff();
 			ledIsOn = false;
