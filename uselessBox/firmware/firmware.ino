8fdb3a63b05c5b09953babedaf7806f6f1defba2
erichstuder <richu_@hotmail.com>
2021-01-24 16:20:58 +0100

    further work for testing

--- uselessBox/firmware/firmware.ino
+++ uselessBox/firmware/firmware.ino
@@ -4,12 +4,26 @@ Servo myservo;
 
 void setup(void) {
 	myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-}
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, HIGH);
 
-void loop(void) {
-	myservo.write(180);
-	delay(3000);
+	const uint8_t SwitchPin = 4;
+	pinMode(SwitchPin, INPUT_PULLUP);
+
+	const uint8_t RelayPin = 12;
+	pinMode(RelayPin, OUTPUT);
+	digitalWrite(RelayPin, LOW);
+	delay(500);
+	digitalWrite(LED_BUILTIN, LOW);
+
 	myservo.write(95);
 	delay(500);
+	myservo.write(180);
+	delay(700);
+
+	while( digitalRead(SwitchPin) == LOW );
+
+	digitalWrite(RelayPin, HIGH);
 }
 
+void loop(void) {}
