f0893c057f021c246eb8fb18746a6e37c6920732
erichstuder <richu_@hotmail.com>
2024-07-06 15:20:13 +0200

    delete, no longer needed

--- UnitTests_withGoogleTest/UnitTests_GoogleTest/driver.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-unsigned char sum;
-
-unsigned char driverAdd(unsigned char a, unsigned char b);
-
-//unsigned char getSum(void);
--- UnitTests_withGoogleTest/dummy.c
+++
@@ -1,6 +0,0 @@
-#include "dummy.h"
-#include "driver.h"
-
-unsigned char add(unsigned char a, unsigned char b){
-	return driverAdd(a,b);
-}
